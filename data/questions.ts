6781bc0a44fda8c15fa628648bf78b6e5434c3e4
hisure2 <om.1634@gmail.com>
2025-04-30 22:32:29 +0530

    first commit

--- data/questions.ts
+++ data/questions.ts
@@ -2,23 +2,23 @@ import type { Question } from "@/types/quiz"
 
 export const questions: Question[] = [
   {
-    text: "What is succinct",
-    options: ["Chain", "Another L2", "Infrastructure"],
-    correctAnswer: "Infrastructure",
+    text: "How succinct vApp is different dApp",
+    options: ["Fully Onchain", "partially onchain & partially offchain", "offchain logic proof sent onchain"],
+    correctAnswer: "offchain logic proof sent onchain",
   },
   {
-    text: "How many stars needed for stage 2",
-    options: ["4000+", "5000+", "7000+"],
-    correctAnswer: "5000+",
+    text: "What makes SP1 different from other ZKVMs like Risc0 or zkSync’s VM",
+    options: ["written in Rust and built with modularity in mind", "prove a specific chain's execution ethereum", "prove a specific chain's execution cosmos"],
+    correctAnswer: "written in Rust and built with modularity in mind",
   },
   {
-    text: "Best way to contribute to succinct",
-    options: ["Active in discord", "Active in 0xcrashout dm", "Active in quiz made by @jefferson"],
-    correctAnswer: "Active in discord",
+    text: "Suppose a developer wants to prove a program’s result to another chain without trusting a centralized party. Which tool is best suited?",
+    options: ["A zk-rollup only on Ethereum", "Succinct SP1", "A multisig bridge"],
+    correctAnswer: "Succinct SP1",
   },
   {
-    text: "Which project is community driven",
-    options: ["Langrange", "Succinct", "Orochi network"],
-    correctAnswer: "Succinct",
+    text: "WIf SP1 proofs are verified on-chain and take less gas than computing the logic directly on-chain, then using SP1 can",
+    options: ["Increase on-chain computation costs", "Lower gas fees and improve scalability", "Slow down the chain"],
+    correctAnswer: "Lower gas fees and improve scalability",
   },
 ]
