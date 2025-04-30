f67d82d903b034a96a8b355063022eb635c337fd
hisure2 <om.1634@gmail.com>
2025-04-30 23:36:15 +0530

    question changed 30042025

--- data/questions.ts
+++ data/questions.ts
@@ -7,12 +7,12 @@ export const questions: Question[] = [
     correctAnswer: "offchain logic proof sent onchain",
   },
   {
-    text: "What makes SP1 different from other ZKVMs like Risc0 or zkSync’s VM",
+    text: "What makes SP1 different from other ZKVMs like Risc0 or zkSyncs VM",
     options: ["written in Rust and built with modularity in mind", "prove a specific chain's execution ethereum", "prove a specific chain's execution cosmos"],
     correctAnswer: "written in Rust and built with modularity in mind",
   },
   {
-    text: "Suppose a developer wants to prove a program’s result to another chain without trusting a centralized party. Which tool is best suited?",
+    text: "Suppose a developer wants to prove a programs result to another chain without trusting a centralized party. Which tool is best suited?",
     options: ["A zk-rollup only on Ethereum", "Succinct SP1", "A multisig bridge"],
     correctAnswer: "Succinct SP1",
   },
